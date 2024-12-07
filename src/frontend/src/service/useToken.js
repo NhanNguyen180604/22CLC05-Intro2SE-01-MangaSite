760ca0ae5784f3b9bb50809ca78d87bd1bb2d6d9
22127228 <lglac22@clc.fitus.edu.vn>
2024-12-07 15:51:42 +0700

    login and register page (I will fix the design later)

---
+++ src/frontend/src/service/useToken.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export default function useToken() {
+  const getToken = () => {
+    const tokenString = localStorage.getItem('token');
+    const timestampString = localStorage.getItem('tokenTimestamp');
+    const userToken = JSON.parse(tokenString);
+    const tokenTimestamp = JSON.parse(timestampString);
+    
+    const expirationTime = 60 * 60 * 1000;
+    const currentTime = new Date().getTime();
+
+    if (currentTime - tokenTimestamp > expirationTime) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('tokenTimestamp');
+      return null;
+    }
+
+    return userToken?.token;
+  };
+
+  const [token, setToken] = useState(getToken());
+
+  const saveToken = userToken => {
+    const currentTime = new Date().getTime();
+    localStorage.setItem('token', JSON.stringify(userToken));
+    localStorage.setItem('tokenTimestamp', JSON.stringify(currentTime));  // Save timestamp
+    setToken(userToken.token);
+  };
+
+  return {
+    setToken: saveToken,
+    token
+  }
+}
--- src/frontend/src/service/userService.js
+++ src/frontend/src/service/userService.js
@@ -1,3 +1,35 @@
+import axios from "axios";
+import { API_URL } from "./service.js";
+const API = API_URL + '/users/';
+
+// get mangas
+export const postLogin = async (email, password) => {
+    try {
+        const response = await axios.post(API + 'login',{
+              email: email,
+              password: password
+          },
+        )
+        return {token: response.data}
+    } catch (error) {
+        return getErrorMessage(error);
+    }
+};
+
+export const postRegister = async (email, username, password) => {
+    try {
+        const response = await axios.post(API + 'register',{
+              email: email,
+              name: username,
+              password: password
+          },
+        )
+        return {user: response.data}
+    } catch (error) {
+        return getErrorMessage(error);
+    }
+};
+
 export async function getMe() {
   return { name: "luna", email: "luna@example.com" };
 
@@ -5,4 +37,18 @@ export async function getMe() {
   // return res.status != 200
   //   ? null
   //   : { name: res.data.name, email: res.data.email };
+}
+
+const getErrorMessage = (error) => {
+  if (error.response) {
+      return {
+          status: error.response.status,
+          message: error.response.data.message
+      }
+  }
+
+  return {
+      status: 500,
+      message: "Something went wrong, and we have no idea"
+  }
 }
