655ca07fd2183ee4e1e09df6746846cc52b4d318
Bao <baolyn187@gmail.com>
2024-12-15 21:48:56 +0700

    feat(home page): noti ui

--- src/frontend/src/pages/HomePage/HomePage.jsx
+++ src/frontend/src/pages/HomePage/HomePage.jsx
@@ -7,16 +7,29 @@ import SearchBox from "../../components/search/SearchBox.jsx"
 import MainLayout from "../../components/main/MainLayout.jsx";
 import MobileNavigationBar from "../../components/main/MobileNavigationBar.jsx";
 import styles from "./HomePage.module.css";
+import { getUserNotifications } from "../../service/userService.js";
 
 // temporary page, whoever does the homepage fix this
 const HomePage = () => {
   const [mangas, setMangas] = useState([]);
+  const [noti, setNoti] = useState(null)
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await getMangas(1, 10);
       if (response.status === 200) {
         setMangas(response.mangas.mangas);
       }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getUserNotifications();
+      console.log(response)
+      setNoti(response)
     };
 
     fetchData();
@@ -38,8 +51,25 @@ const HomePage = () => {
             <MangaSlide title="New mangas" mangas={mangas} />
             {/* <MangaSlide title="Top rated" mangas={mangas} /> */}
           </div>
+
           <div className={styles.notification}>
             <h4 className={styles.notificationHeader}>Notifications</h4>
+
+            {noti ? (
+              noti.map((noti) => {
+                const createdAt = new Date(noti.createdAt);
+                const date = createdAt.toLocaleDateString();
+                const time = createdAt.toLocaleTimeString();
+                return (
+                  <div className={styles.notiBox} key={noti._id}>
+                    <p className={styles.notiMessage}>{noti.message}</p>
+                    <span className={styles.notiTime}>{`${date} ${time}`}</span>
+                  </div>
+                );
+              })
+            ) : (
+              <p>Please login to see your notifications</p>
+            )}
           </div>
         </div>
 
